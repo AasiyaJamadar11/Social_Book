c5e3c3f03e571fd9ab3a8b82b96674f267da8133
AasiyaJamadar <aasiyajamadar5@gmail.com>
2025-01-10 18:51:41 +0530

    .

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,9 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class User(AbstractUser):
+    public_visibility = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,12 @@ from django.urls import path,include
 
 from .views import register
 from .views import user_login
+from . import views
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
+    path('authors-and-sellers/', views.authors_and_sellers, name='authors_and_sellers'),
    
 ]
 
